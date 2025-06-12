85fc754b751121b6c29f1218ac41da839e6051f7
Robert <robert.hartono.025@gmail.com>
2025-06-12 08:05:25 +0700

    Add Reel animation

--- assets/Scripts/Reel.ts
+++ assets/Scripts/Reel.ts
@@ -1,32 +1,95 @@
-import { _decorator, Component, Node, EventTarget } from 'cc';
+import { _decorator, Component, Node, EventTarget, Vec3, tween, Layout } from 'cc';
 const { ccclass, property } = _decorator;
-
-const EventEmitter = new EventTarget(); 
 
 @ccclass('Reel')
 export class Reel extends Component 
 {
+    public EventEmitter = new EventTarget();
+
     public static Events =
     {
         SPINCOMPLETE: "SpinComplete"
     };
 
     @property
-    private spinSpeed: number = 5;
+    private spinSpeedMin: number = 1;
+    @property
+    private spinSpeedMax: number = 1;
+    @property
+    private spinDuration: number = 5;
+    @property
+    private itemSpacing: number = 5;
 
-    public Spin()
+    private reelItems: Node[] = [];
+    private isSpinning = false;
+    private spinTimer = 0;
+    private spinSpeed = 0;
+
+    protected onLoad(): void 
     {
+        this.reelItems = this.node.children.slice();
 
     }
 
-    public static SubscribeSpinComplete(callback: () => void, target?: any)
+    public Spin(times: number = 10)
     {
-        EventEmitter.on(Reel.Events.SPINCOMPLETE, callback, target);
+        this.isSpinning = true;
+        this.spinTimer = 0;
+        this.spinSpeed = Math.random() * (this.spinSpeedMax - this.spinSpeedMin) + this.spinSpeedMin;
+        this.schedule(this.SpinReel, 0);
+    }
+
+    private SpinReel(deltaTime: number = .016)
+    {
+        this.spinTimer += deltaTime;
+
+        if (this.isSpinning == false || this.spinTimer >= this.spinDuration)
+        {
+            this.isSpinning = false;
+            this.unschedule(this.SpinReel);
+            this.Snap();
+            this.EventEmitter.emit(Reel.Events.SPINCOMPLETE);
+            return;
+        }
+
+        for (let item of this.reelItems)
+        {
+            let pos = item.getPosition();
+            pos.y -= this.spinSpeed * .016;
+            item.setPosition(pos);
+        }
+
+        const topItem = this.reelItems[0];
+        const botItem = this.reelItems[this.reelItems.length - 1];
+        if (botItem.getPosition().y < -this.itemSpacing * 3) 
+        {
+            const newY = topItem.getPosition().y + this.itemSpacing;
+            botItem.setPosition(0, newY);
+            
+            // Move to end of the list
+            this.reelItems.pop();
+            this.reelItems.unshift(botItem);
+        }
+    }
+
+    private Snap()
+    {
+        const top = this.itemSpacing * (this.reelItems.length - 1) / 2;
+        for (let i = 0; i < this.reelItems.length; i++)
+        {
+            const y = top - i * this.itemSpacing;
+            this.reelItems[i].setPosition(0, y);   
+        } 
+    }
+
+    public SubscribeSpinComplete(callback: () => void, target?: any)
+    {
+        this.EventEmitter.on(Reel.Events.SPINCOMPLETE, callback, target);
     }
     
-    public static UnSubcribeeSpinComplete(callback: () => void, target?: any)
+    public UnSubcribeSpinComplete(callback: () => void, target?: any)
     {
-        EventEmitter.off(Reel.Events.SPINCOMPLETE, callback, target);
+        this.EventEmitter.off(Reel.Events.SPINCOMPLETE, callback, target);
     }
 }
 
--- assets/Scripts/ReelController.ts
+++ assets/Scripts/ReelController.ts
@@ -6,38 +6,67 @@ const { ccclass, property } = _decorator;
 const EventEmitter = new EventTarget(); 
 
 @ccclass('ReelController')
-export class ReelController extends Component 
+export class ReelController extends Component  implements ICanInitialize
 {
     public static Events =
     {
+        ONREELSTART: "OnReelStart",
         ONREELCOMPLETE: "OnReelComplete"
     };
-
+    
     @property(Reel)
     private reelLeft: Reel;
-
+    
     @property(Reel)
     private reelMid: Reel;
-
+    
     @property(Reel)
     private reelRight: Reel;
-
+    
     @property
     private reelInterval: number = 1;
-
+    
     start() 
     {
-
+        this.reelLeft.SubscribeSpinComplete(this.OnLeftFinish);
+        this.reelMid.SubscribeSpinComplete(this.OnMiddleFinish);
+        this.reelRight.SubscribeSpinComplete(this.OnRightFinish);
     }
-
+    
     update(deltaTime: number) 
     {
         
     }
 
-    public Spin()
+    Initialize(value: IData): void 
     {
+        
+    }
 
+    public async Spin()
+    {
+        this.reelLeft.Spin(5);
+        
+        await this.Delay(1000);
+        this.reelMid.Spin(5);
+        
+        await this.Delay(1000);
+        this.reelRight.Spin(5);
+    }
+
+    private Delay(ms: number)
+    {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    public SubscribeSpinStart(callback: (value: ReelResult) => void, target?: any)
+    {
+        EventEmitter.on(ReelController.Events.ONREELSTART, callback, target);
+    }
+    
+    public UnSubcribeeSpinStart(callback: (value: ReelResult) => void, target?: any)
+    {
+        EventEmitter.off(ReelController.Events.ONREELSTART, callback, target);
     }
 
     public SubscribeSpinComplete(callback: (value: ReelResult) => void, target?: any)
@@ -48,6 +77,21 @@ export class ReelController extends Component
     public UnSubcribeeSpinComplete(callback: (value: ReelResult) => void, target?: any)
     {
         EventEmitter.off(ReelController.Events.ONREELCOMPLETE, callback, target);
+    }
+
+    private OnLeftFinish()
+    {
+        console.log("LEFT FINISH");
+    }
+
+    private OnMiddleFinish()
+    {
+        console.log("MIDDLE FINISH");
+    }
+
+    private OnRightFinish()
+    {
+        console.log("RIGHT FINISH");
     }
 }
 
