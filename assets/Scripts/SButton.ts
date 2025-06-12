7c564ab60c802d50fa95aa43fbcf4e9ff45c4e2b
Robert <robert.hartono.025@gmail.com>
2025-06-12 09:59:18 +0700

    Implement OnClick

--- assets/Scripts/SButton.ts
+++ assets/Scripts/SButton.ts
@@ -1,11 +1,11 @@
 import { _decorator, Component, Node, EventTarget } from 'cc';
 const { ccclass, property } = _decorator;
 
-const EventEmitter = new EventTarget(); 
-
 @ccclass('SButton')
 export class SButton extends Component 
 {
+    private EventEmitter = new EventTarget(); 
+    
     public static Events =
     {
         ONCLICK: "OnClick"
@@ -13,17 +13,17 @@ export class SButton extends Component
 
     public Click()
     {
-
+        this.EventEmitter.emit(SButton.Events.ONCLICK);
     }
 
     public SubscribeSpinComplete(callback: () => void, target?: any)
     {
-        EventEmitter.on(SButton.Events.ONCLICK, callback, target);
+        this.EventEmitter.on(SButton.Events.ONCLICK, callback, target);
     }
     
     public UnSubcribeeSpinComplete(callback: () => void, target?: any)
     {
-        EventEmitter.off(SButton.Events.ONCLICK, callback, target);
+        this.EventEmitter.off(SButton.Events.ONCLICK, callback, target);
     }
 }
 
